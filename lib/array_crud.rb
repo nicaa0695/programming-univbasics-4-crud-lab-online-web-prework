fae1914b5e9dcdc601edee994a1420abe4d5b96a
Veronica Lopez <nicaa0695@gmail.com>
2019-07-02 19:28:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.push("arrays!")
+  create_an_array.push("wow")
 end
 
 def remove_element_from_end_of_array(array)
